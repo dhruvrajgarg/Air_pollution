c22f268d17191e9990f554e894465b18b42de554
dhruvrajgarg <dhruv.rajgarg@ymail.com>
2018-06-27 23:42:16 +0530

    added pie chart functionality

--- pollution.py
+++ pollution.py
@@ -14,4 +14,12 @@ print(filename)
 filename = './Data/20180626-23.csv'
 #Load Data into a pandas dataframe
 data_df = pd.read_csv(filename)
-print(data_df['station'== 'Adarsh Nagar, Jaipur - RSPCB'])
+
+#Plotting various pollutants as a pie chart for a city at a particular time 
+
+city_df = data_df[data_df['station'] == "Secretariat, Amaravati - APPCB"]
+print(city_df.head())
+for pollutant in city_df:
+    plt.pie(city_df['pollutant_avg'],labels=city_df['pollutant_id'],autopct = '%0.2f%%',shadow= True)
+plt.axis('equal')
+plt.show()
